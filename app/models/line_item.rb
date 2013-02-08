149d8fd2e67fcccb35c80a3188eb12bd668b50ab
Szymon Fiedler <fidelski@gmail.com>
2013-02-08 16:30:22 +0100

    Task E: A Smarter Cart

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -10,4 +10,8 @@ class LineItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :cart
   attr_accessible :cart_id, :product_id
+
+  def total_price
+    product.price * quantity
+  end
 end
